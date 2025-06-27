3f8e95f7d0ab739f1902e9aa811c828373cc808e
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2025-06-27 15:14:10 -0400

    Add Next.js configuration for static export

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    forceSwcTransforms: true,
+  },
+}
+
+module.exports = nextConfig
